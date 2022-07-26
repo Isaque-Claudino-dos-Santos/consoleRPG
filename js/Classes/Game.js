1ae9237c9e330633a31b94f1935a491b5563f29a
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2022-07-26 17:58:22 -0300

    class to of game

---
+++ js/Classes/Game.js
@@ -0,0 +1,12 @@
+import Player from "./Player.js"
+
+class Game {
+    constructor(player) {
+        if (!(player instanceof Player)) throw 'The constructor param not is a Player instance'
+
+        
+
+    }
+}
+
+export default Game
