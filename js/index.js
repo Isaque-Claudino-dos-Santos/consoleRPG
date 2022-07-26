0ec16f0a840cc27d2015bd5d60c163e5cf92e4e0
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2022-07-26 17:57:46 -0300

    call class game

--- js/index.js
+++ js/index.js
@@ -1,8 +1,14 @@
 import Player from "./Classes/Player.js";
 import Enimy from "./Classes/Enimy.js";
+import Game from "./Classes/Game.js";
+
 
 let player = new Player('isaque', 10, 2, 0)
 let slime = new Enimy('slime', 5, 1, 0)
 
+player.strick(slime)
 
-console.table([slime, player])
+let game = new Game(player)
+
+console.table(player)
+console.table(slime)
