23437b6dffc48f46eabd8739c9fd918869fcb24c
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2022-07-28 12:37:18 -0300

    defaut file

--- js/index.js
+++ js/index.js
@@ -1,14 +1,10 @@
-import Player from "./Classes/Entitys/Player.js";
-import Enimy from "./Classes/Entitys/Enimy.js";
-import Game from "./Classes/Game.js";
-import Coins from "./Classes/Items/Coins.js";
+"use strict";
 
-let c = new Coins('coins', 'Use to negotiation in the market', 1)
+import store from "./store.js";
 
-let player = new Player('isaque', 10, 2, 0)
-let game = new Game(player)
 
-player.getItem(c)
-player.getItem(c)
 
-console.log(player)
+
+console.log(store)
+
+
