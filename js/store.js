8c5c615a59c07184a71a0426fca542d4eea0ca3b
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2022-07-28 17:54:53 -0300

    add sword item

--- js/store.js
+++ js/store.js
@@ -1,21 +1,29 @@
 import Player from "./Classes/Entitys/Player.js"
 import Enimy from "./Classes/Entitys/Enimy.js"
 import Coins from "./Classes/Items/Coins.js"
+import Sword from "./Classes/Items/Sword.js"
 
 
 const players = []
 
-players.push(new Player('isaque', 10, 3, 0))
+players.push(
+    new Player('isaque', 10, 3, 0)
+)
 
 
 const items = []
 
-items.push(new Coins('coins', 'Use to negotiation in the market', 1))
+items.push(
+    new Coins('coins', 'Use to negotiation in the market', 1),
+    new Sword('smal sword', 'Increment Attack of the player', 2, 1)
+)
 
 
 const enimys = []
 
-enimys.push(new Enimy('slime', 5, 2, 0))
+enimys.push(
+    new Enimy('slime', 5, 2, 0)
+)
 
 function storeFind(name, array) {
     let obj = array.filter(value => { return value.name === name })[0]
