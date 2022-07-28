169e1f87882beb3c3fe929088f249133892cdb61
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2022-07-28 14:02:49 -0300

    add type player

--- js/Classes/Entitys/Player.js
+++ js/Classes/Entitys/Player.js
@@ -3,6 +3,7 @@ import Entity from "./Entity.js";
 class Player extends Entity {
     constructor(name, life, attack, defense) {
         super(name, life, attack, defense)
+        this.type = 'player'
     }
 }
 
