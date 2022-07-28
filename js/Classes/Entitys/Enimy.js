fce895b23157677a65c25085b9916a8a27a1a8c8
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2022-07-28 14:02:55 -0300

    add type enimy

--- js/Classes/Entitys/Enimy.js
+++ js/Classes/Entitys/Enimy.js
@@ -3,6 +3,7 @@ import Entity from "./Entity.js";
 class Enimy extends Entity {
     constructor(name, life, attack, defense) {
         super(name, life, attack, defense)
+        this.type = 'enimy'
     }
 }
 
