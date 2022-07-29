83b1b809f61b52595928b8220226801866994fa9
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2022-07-29 19:37:20 -0300

    use terminal and export comands

--- js/index.js
+++ js/index.js
@@ -1,5 +1,7 @@
 "use strict";
 
+import comand from './Classes/Terminal/comand.js';
+import Terminal from './Classes/Terminal/Terminal.js'
 import { players, enimys, items, storeFind } from "./store.js";
 
 
@@ -9,7 +11,13 @@ p.getItem(storeFind('coins', items))
 p.getItem(storeFind('smal sword', items))
 
 
+let terminal = new Terminal()
 
-p.element('body')
+terminal.execute = () => {
+    let req = terminal.request
+    let res = comand.call(req.comand, req.params)
+
+    terminal.setResponse(res)
+}
 
 
