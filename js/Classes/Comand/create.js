70bca5ca88b996a06c7a2415ac599a078c5683c1
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2022-07-29 23:06:16 -0300

    comando game criado

--- js/Classes/Comand/create.js
+++ js/Classes/Comand/create.js
@@ -2,6 +2,8 @@ import Comand from './Comand.js'
 
 let comand = new Comand()
 
+//comands to the consoles
+
 comand.create('echo', (params) => {
     return params.join(' ')
 }, 'Use echo [mensage] to write console')
@@ -33,6 +35,33 @@ comand.create('prompt', (params) => {
 }, 'exec prompt [label] box with input in the browser')
 
 
+//comands to the Game
+
+comand.create('game', (params) => {
+    let obj = params[0]
+    let method = params[1]
+    let param = params[2]
+
+    let game = {}
+
+    let call = {
+        'state': {
+            'start': () => {
+                return 'start game OK'
+            }
+        },
+        'select': {
+            'player': (namePlayer) => {
+                return namePlayer
+            }
+        }
+    }
+
+    if (obj in call && method !== undefined)
+        return call[obj][method](param)
+
+    return ''
+}, 'game terminal em desenvolvimento ...')
 
 
 
